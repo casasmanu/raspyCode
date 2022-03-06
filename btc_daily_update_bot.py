0afb24c1305f9460045bb370027007b0422d3aee
Manuel Maria Casas <mcasas@itba.edu.ar>
2022-03-05 23:24:57 -0300

    version2

--- btc_daily_update_bot.py
+++ btc_daily_update_bot.py
@@ -6,8 +6,8 @@ import schedule
 def bot_send_text(bot_message):
     
     bot_token = '5212795032:AAH32kz9IJlgoXZZFOZcQz6RfshGiWXxb_w'
-    #bot_chatID = '5178063489' #mio
-    bot_chatID = '220619299' #Jose
+    bot_chatID = '5178063489' #mio
+    #bot_chatID = '220619299' #Jose
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
